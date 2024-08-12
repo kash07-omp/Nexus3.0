3fa0958787a9fdedcaaf2a058311224a8b55dffe
Kash <oscar.martin.pardo94@gmail.com>
2024-08-12 17:21:18 +0200

    Inject repositories in program

--- Nexus/Nexus.Web/Program.cs
+++ Nexus/Nexus.Web/Program.cs
@@ -1,7 +1,37 @@
+using Nexus.Data.Repositories;
+using Nexus.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// services configuration
+// Services
+builder.Services.AddScoped<IPlanetService, PlanetService>();
+builder.Services.AddScoped<IRegionService, RegionService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IStructureService, StructureService>();
+builder.Services.AddScoped<IMineService, MineService>();
+builder.Services.AddScoped<IResourceService, ResourceService>();
+builder.Services.AddScoped<IRegionStructureService, RegionStructureService>();
+
+// Repositories
+builder.Services.AddScoped<IPlanetRepository, PlanetRepository>();
+builder.Services.AddScoped<IRegionRepository, RegionRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IStructureRepository, StructureRepository>();
+builder.Services.AddScoped<IMineRepository, MineRepository>();
+builder.Services.AddScoped<IResourceRepository, ResourceRepository>();
+builder.Services.AddScoped<IRegionStructureRepository, RegionStructureRepository>();
+
+// Use scrutor for scaning and register services and repositories
+//builder.Services.Scan(scan => scan
+//    .FromAssemblyOf<IPlanetService>()
+//    .AddClasses()
+//    .AsImplementedInterfaces()
+//    .WithScopedLifetime());
+
 
 var app = builder.Build();
 
